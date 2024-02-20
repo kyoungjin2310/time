7ae9f3aebf13c72d996237536b28cfdc92fed4cb
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-02-20 16:12:18 +0900

    feat: 로그인 추가

---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import InputStyle from "@/app/_component/style/input/InputStyle";
+import { NextPage } from "next";
+import { useState } from "react";
+
+interface Props {
+  title: string;
+  id: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  style: string;
+}
+
+const Page: NextPage<Props> = ({}) => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {};
+
+  const inputId: Props = {
+    title: "ID",
+    id: "id",
+    type: "text",
+    placeholder: "아이디를 입력해주세요.",
+    value: id,
+    style: "",
+  };
+
+  const inputPassword: Props = {
+    title: "Password",
+    id: "password",
+    type: "password",
+    placeholder: "패스워드를 입력해주세요.",
+    value: password,
+    style: "",
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset>
+        <div>
+          <InputStyle input={inputId} onChangeValue={setId} />
+          <InputStyle input={inputPassword} onChangeValue={setPassword} />
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Page;
