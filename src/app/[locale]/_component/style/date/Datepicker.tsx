8ee2be33c2f8d1fd22a564bcc1af5fe0998faeec
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-03-12 17:51:29 +0900

    feat: 달력 css

--- src/app/[locale]/_component/style/date/Datepicker.tsx
+++ src/app/[locale]/_component/style/date/Datepicker.tsx
@@ -4,15 +4,28 @@ import ReactDatePicker from "react-datepicker";
 import ko from "date-fns/locale/ko";
 
 import "./datepicker.mobule.css";
+import { addMonths, subMonths } from "date-fns";
 
 const Datepicker = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
-    <ReactDatePicker
-      selected={startDate}
-      onChange={(date) => date && setStartDate(date)}
-      inline
-    />
+    <div>
+      <div className="title">
+        <ReactDatePicker
+          selected={startDate}
+          onChange={(date) => date && setStartDate(date)}
+          dateFormat="MMMM yyyy"
+          showMonthYearPicker
+        />
+      </div>
+      <div className="main">
+        <ReactDatePicker
+          selected={startDate}
+          onChange={(date) => date && setStartDate(date)}
+          inline
+        />
+      </div>
+    </div>
   );
 };
 
