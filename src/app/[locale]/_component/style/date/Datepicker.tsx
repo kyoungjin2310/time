cac85d1c069673bcb92aa0549c088a570e2b1f57
kyoungjin2310 <kyoungjin2310@gmail.com>
2024-03-13 17:01:35 +0900

    feat: 달력에 언어 적용

--- src/app/[locale]/_component/style/date/Datepicker.tsx
+++ src/app/[locale]/_component/style/date/Datepicker.tsx
@@ -1,27 +1,50 @@
 "use client";
 import { useState } from "react";
 import ReactDatePicker from "react-datepicker";
-import ko from "date-fns/locale/ko";
+import { useCurrentLocale } from "@/app/messages/client";
+import { ko } from "date-fns/locale";
 
 import "./datepicker.mobule.css";
-import { addMonths, subMonths } from "date-fns";
+import { subDays } from "date-fns";
+import { forwardRef } from "@nextui-org/react";
 
 const Datepicker = () => {
+  const locales = useCurrentLocale();
   const [startDate, setStartDate] = useState(new Date());
+  const [locale, seLocale] = useState(locales == "kr" ? ko : locales);
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button className="custom-input" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+  const highlightWithRanges: any = [
+    {
+      "react-datepicker__day--highlighted-custom-1": [
+        subDays(new Date(), 4),
+        subDays(new Date(), 3),
+        subDays(new Date(), 2),
+        subDays(new Date(), 1),
+      ],
+    },
+  ];
   return (
     <div>
       <div className="title">
         <ReactDatePicker
+          locale={locale}
           selected={startDate}
           onChange={(date) => date && setStartDate(date)}
           dateFormat="MMMM yyyy"
           showMonthYearPicker
+          customInput={<CustomInput />}
         />
       </div>
       <div className="main">
         <ReactDatePicker
           selected={startDate}
           onChange={(date) => date && setStartDate(date)}
+          onMonthChange={(date) => date && setStartDate(date)}
+          highlightDates={highlightWithRanges}
           inline
         />
       </div>
